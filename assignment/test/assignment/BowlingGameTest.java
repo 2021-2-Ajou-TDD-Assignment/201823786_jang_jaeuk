7338d852c5062cea4085ae89549419c16542e10b
Jaeuk <enrtja5980@ajou.ac.kr>
2021-10-12 23:32:39 +0900

    The Third Test

--- assignment/test/assignment/BowlingGameTest.java
+++ assignment/test/assignment/BowlingGameTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BowlingGameTest {
@@ -31,6 +32,17 @@ public class BowlingGameTest {
 	public void testAllOnes() {
 		rollMany(20, 1);
 		assertEquals(20,g.score());
+	}
+	
+	@Ignore("Until we get design right")
+	
+	@Test
+	public void testOneSpare() {
+		g.roll(5);
+		g.roll(5);
+		g.roll(3);
+		rollMany(17, 0);
+		assertEquals(16,g.score());
 	}	
 
 }
